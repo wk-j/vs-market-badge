7738de67c8c568911dbfb64b2220f4068c75a2e3
wk-j <somnuk.wk@gmail.com>
2019-02-16 03:36:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Visual Studio Marketplace Badge
+
+[![NuGet](https://img.shields.io/nuget/v/wk.VsMarketBadge.svg)](https://www.nuget.org/packages/wk.VsMarketBadge)
+
+## Installation
+
+```bash
+dotnet tool install -g wk.VsMarketBadge
+```
+
+## Usage
+
+```bash
+wk-vs-market-badge <Extension Name>
+```
---
+++ src/VsMarketBadge/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let template name =
+    let template =
+        """
+        [![Version](https://vsmarketplacebadge.apphb.com/version/{name}.svg)](https://marketplace.visualstudio.com/items?itemName={name}) [![Installs](https://vsmarketplacebadge.apphb.com/installs-short/{name}.svg)](https://marketplace.visualstudio.com/items?itemName={name})
+        """
+    template.Replace("{name}", name).Trim()
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| name |] ->
+        printfn "%s" (template name)
+    | _ ->
+        printfn "use 'wk-vs-market-badge <name>'"
+    0 // return an integer exit code
